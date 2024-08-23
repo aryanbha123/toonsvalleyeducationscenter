c7486e6c5c6094c5df39cb5695e387a5861196ef
Aaryan Bhandari <aryanbhandari4077@gmail.com>
2024-08-23 21:49:28 +0530

    push

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express();
-const port = 8000 || process.env.PORT 
+const port = 8000 || process.env.PORT
+const Uri =  "mongodb+srv://aryanbhandari4077:<password>@cluster0.wqexvgn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" || process.env.URI 
+const mongoose = require('mongoose')
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors({
+    origin:"*",
+    allowedHeaders:true
+}));
+
 
 app.get('/' , (req,res) => {
     res.send("Server Live ");
@@ -9,3 +19,7 @@ app.get('/' , (req,res) => {
 app.listen(port , () => {
     console.log("Form Connected")
 });
+
+mongoose.connect(URI , (req,res) =>  {
+
+})
