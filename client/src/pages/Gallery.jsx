7ccefe0224070592128994b1930230bfa9718647
Pranjal-Rawat <145280526+Pranjal-Rawat@users.noreply.github.com>
2024-08-31 21:32:43 +0530

    Update Gallery.jsx

--- client/src/pages/Gallery.jsx
+++ client/src/pages/Gallery.jsx
@@ -19,11 +19,11 @@ import g22 from './gallery/22.jpg';
 import g31 from '../assets/IMG-20240823-WA0009.jpg';
 import g30 from '../assets/IMG-20190424-WA0005.jpg';
 import g29 from '../assets/IMG-20240824-WA0000.jpg';
-import g24 from '../assets/24.jpg';
-import g25 from '../assets/25.jpg';
-import g26 from '../assets/26.jpg';
-import g27 from '../assets/27.jpg';
-import g28 from '../assets/28.jpg';
+import g24 from './gallery/24.jpg';
+import g25 from './gallery/25.jpg';
+import g26 from './gallery/26.jpg';
+import g27 from './gallery/27.jpg';
+import g28 from './gallery/28.jpg';
 
 const images = {
   All: [g1, g2, g3, g4, g5, g6, g7, g8, g10, g11, g14, g17, g20, g21, g22, g24, g25, g26, g27, g28, g29, g30, g31],
